22ac1ef27cb5df666c1138a66598184608850648
Jannatul <limajannat1675@gmail.com>
2025-03-15 20:11:17 +0600

    added categories section

--- js/main.js
+++ js/main.js
@@ -5,14 +5,17 @@ const loadCategory = async() => {
     showCategory(data.categories)
 }
 
+// display categories on UI
 const showCategory = (categories) => {
     categories.forEach((element) => {
         console.log(element);
         const categoryContainer = document.getElementById('category-container');
         const div = document.createElement("div");
+
+        // show category icon on button
         div.innerHTML = `
-        <button class="btn">${element.category}
-        <img class="w-8" src=${element.category_icon} alt="">   
+        <button class="btn text-2xl px-2 w-[312px] h-[104px] hover:bg-[#0E7A81]/10 hover:rounded-full">${element.category}
+        <img class="w-12" src=${element.category_icon} alt="">   
         </button>
         `
         categoryContainer.appendChild(div)
